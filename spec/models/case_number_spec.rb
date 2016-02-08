65b2c2272ed39f03272df36edc8eb6bcd0881aa4
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2016-02-08 15:04:13 +0000

    CaseNumber model added
    It's meant to model the form which handles the question:
    
    "Do you have a case, claim or ‘notice to pay’ number?"

---
+++ spec/models/case_number_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe CaseNumber, type: :model do
+  subject { described_class.new }
+
+  describe 'validations' do
+    describe 'number' do
+      context 'when true' do
+        before { subject.number = true }
+
+        it { expect(subject.valid?).to be true }
+      end
+
+      context 'when false' do
+        before { subject.number = false }
+
+        it { expect(subject.valid?).to be true }
+      end
+
+      context 'when not a boolean value' do
+        before { subject.number = 'string' }
+
+        it { expect(subject.valid?).to be false }
+      end
+    end
+  end
+end

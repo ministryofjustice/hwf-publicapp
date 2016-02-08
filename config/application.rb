c108c2087af0d63f5d334a66ce6a1d9b9c34af79
axemonkey <hi+github@clivemurray.com>
2016-02-08 14:04:52 +0000

    add frontend toolkit, import stylesheets

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,13 @@ module HwfPublicapp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.app_title = 'Help with fees'
+    config.proposition_title = 'Help with fees'
+    config.product_type = 'service'
+
+    # The following values are required by the phase banner
+    config.phase = 'alpha'
+    config.feedback_url = '#'
   end
 end

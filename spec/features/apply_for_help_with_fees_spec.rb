058488b880b85cb3570f9b106405605d58d3468d
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2016-02-11 15:33:52 +0000

    Reworked the feature steps
    There is a bit of repetition, but for now it's OK to do that

--- spec/features/apply_for_help_with_fees_spec.rb
+++ spec/features/apply_for_help_with_fees_spec.rb
@@ -3,55 +3,55 @@ require 'rails_helper'
 
 RSpec.feature 'As a user' do
   scenario 'I want to start the application for "Help with fees"' do
-    start_the_application
+    page.visit '/'
+    page.click_link 'Apply now'
   end
 
   scenario 'I want to add my marital status' do
-    enter_marital_status
-  end
-
-  scenario 'I want to add information on my savings and investments' do
-    enter_savings_and_investments
-  end
-
-  scenario 'I want to see the summary of my application' do
-    application_summary
-  end
-
-  def start_the_application
-    visit '/'
-    click_link 'Apply now'
-  end
-
-  def enter_marital_status
-    start_the_application
+    page.visit '/'
+    page.click_link 'Apply now'
     expect(page).to have_content "What's your status?"
     choose 'marital_status_married_false'
     click_button 'Continue'
   end
 
-  def enter_savings_and_investments
-    start_the_application
-    enter_marital_status
+  scenario 'I want to add information on my savings and investments' do
+    page.visit '/'
+    page.click_link 'Apply now'
+    expect(page).to have_content "What's your status?"
+    choose 'marital_status_married_false'
+    click_button 'Continue'
     expect(page).to have_content 'How much do you have in savings and investments?'
     choose 'savings_and_investment_less_than_limit_false'
     click_button 'Continue'
   end
 
-  def enter_benefits_info
-    start_the_application
-    enter_marital_status
-    enter_savings_and_investments
+  scenario 'I want to add information on benefits' do
+    page.visit '/'
+    page.click_link 'Apply now'
+    expect(page).to have_content "What's your status?"
+    choose 'marital_status_married_false'
+    click_button 'Continue'
+    expect(page).to have_content 'How much do you have in savings and investments?'
+    choose 'savings_and_investment_less_than_limit_false'
+    click_button 'Continue'
     expect(page).to have_content 'Do you receive any of the following benefits?'
     choose 'benefit_on_benefits_false'
     click_button 'Continue'
   end
 
-  def application_summary
-    start_the_application
-    enter_marital_status
-    enter_savings_and_investments
-    enter_benefits_info
+  scenario 'I want to see the summary of my application' do
+    page.visit '/'
+    page.click_link 'Apply now'
+    expect(page).to have_content "What's your status?"
+    choose 'marital_status_married_false'
+    click_button 'Continue'
+    expect(page).to have_content 'How much do you have in savings and investments?'
+    choose 'savings_and_investment_less_than_limit_false'
+    click_button 'Continue'
+    expect(page).to have_content 'Do you receive any of the following benefits?'
+    choose 'benefit_on_benefits_false'
+    click_button 'Continue'
     expect(page).to have_content 'Check details'
     expect(page).to have_content 'Single'
     expect(page).to have_content 'Less than £3,000'

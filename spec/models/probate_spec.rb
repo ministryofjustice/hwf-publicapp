272674db4065afc3e19637ad91127de7942854d9
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2016-02-11 20:55:42 +0000

    Renamed ProbateCaseFee model to Probate
    Seems like a better naming, so changing it

--- spec/models/probate_spec.rb
+++ spec/models/probate_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe ProbateCaseFee, type: :model do
+RSpec.describe Probate, type: :model do
   subject { described_class.new }
 
   describe 'validations' do
-    describe 'answer' do
+    describe 'probate_case' do
       context 'when true' do
-        before { subject.answer = true }
+        before { subject.probate_case = true }
 
         it { expect(subject.valid?).to be true }
       end
 
       context 'when false' do
-        before { subject.answer = false }
+        before { subject.probate_case = false }
 
         it { expect(subject.valid?).to be true }
       end
 
       context 'when not a boolean value' do
-        before { subject.answer = 'string' }
+        before { subject.probate_case = 'string' }
 
         it { expect(subject.valid?).to be false }
       end

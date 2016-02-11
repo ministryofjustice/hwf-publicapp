94e3a0f7569ebd638480c91388cd95847dd72b6f
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2016-02-11 21:43:53 +0000

    Added the form for adding Probate case
    Added the examples for both show & save controller & views
    
    Haven't added the extra attributes on the Probate model, as I'd like to
    get the happy path going for now.

--- spec/features/apply_for_help_with_fees_spec.rb
+++ spec/features/apply_for_help_with_fees_spec.rb
@@ -57,6 +57,26 @@ RSpec.feature 'As a user' do
     click_button 'Continue'
   end
 
+  scenario 'I want to add information on probate case' do
+    page.visit '/'
+    page.click_link 'Apply now'
+    expect(page).to have_content "What's your status?"
+    choose 'marital_status_married_false'
+    click_button 'Continue'
+    expect(page).to have_content 'How much do you have in savings and investments?'
+    choose 'savings_and_investment_less_than_limit_false'
+    click_button 'Continue'
+    expect(page).to have_content 'Do you receive any of the following benefits?'
+    choose 'benefit_on_benefits_false'
+    click_button 'Continue'
+    expect(page).to have_content 'Have you already paid the fee?'
+    choose 'fee_paid_false'
+    click_button 'Continue'
+    expect(page).to have_content 'Are you paying a fee for a probate case?'
+    choose 'probate_kase_false'
+    click_button 'Continue'
+  end
+
   scenario 'I want to see the summary of my application' do
     page.visit '/'
     page.click_link 'Apply now'
@@ -72,10 +92,14 @@ RSpec.feature 'As a user' do
     expect(page).to have_content 'Have you already paid the fee?'
     choose 'fee_paid_false'
     click_button 'Continue'
+    expect(page).to have_content 'Are you paying a fee for a probate case?'
+    choose 'probate_kase_false'
+    click_button 'Continue'
     expect(page).to have_content 'Check details'
     expect(page).to have_content 'Single'
     expect(page).to have_content 'Less than £3,000'
     expect(page).to have_content 'Not receiving eligible benefits'
     expect(page).to have_content 'Fee paidNo'
+    expect(page).to have_content 'Probate caseNo'
   end
 end

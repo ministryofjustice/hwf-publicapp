9cb4a108bff49f2e9f1651a5438d4db7214859ec
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-02-15 14:18:02 +0000

    Updated after content review

--- spec/features/pages/question_one_spec.rb
+++ spec/features/pages/question_one_spec.rb
@@ -26,7 +26,7 @@ RSpec.feature 'As a user' do
       end
 
       scenario 'I expect the fields to have specific errors' do
-        expect(page).to have_xpath('//span[@class="error-message"]', text: 'Enter your marital status')
+        expect(page).to have_xpath('//span[@class="error-message"]', text: 'Select your marital status')
       end
     end
   end

c9ede9595c044c3de1bff808347c06e2e4512928
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2016-02-16 20:26:35 +0000

    Added phone contact part to Contact model
    The Contact model already has email part, now added the phone part.
    
    For validation, I've omitted any proper/strict validation and just left
    it as 'the phone number field should not be blank'. Will revisit it
    later, when I've spoke with others in the team.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,12 +1,19 @@
 class Contact < Base
   attribute :email_option, Boolean
   attribute :email, String
+  attribute :phone_option, Boolean
+  attribute :phone, String
 
   validates :email_option, inclusion: { in: [true, false] }
+  validates :phone_option, inclusion: { in: [true, false] }
 
   with_options if: :email_option? do
     email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     validates :email, format: { with: email_regex, allow_nil: false }
+  end
+
+  with_options if: :phone_option? do
+    validates :phone, presence: true
   end
 
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  subject { described_class.new }
+  subject do
+    described_class.new(email_option: true,
+                        email: 'foo@bar.com',
+                        phone_option: true,
+                        phone: '078 112 123 22')
+  end
 
   describe 'validations' do
-    before do
-      subject.email_option = true
-      subject.email = 'foo@bar.com'
-    end
-
     describe 'email_option' do
       context 'when true' do
         it { expect(subject.valid?).to be true }
@@ -28,6 +28,32 @@ RSpec.describe Contact, type: :model do
 
           context 'and email is not given' do
             before { subject.email = '' }
+
+            it { expect(subject.valid?).to be false }
+          end
+        end
+      end
+    end
+
+    describe 'phone_option' do
+      context 'when true' do
+        it { expect(subject.valid?).to be true }
+      end
+
+      context 'when false' do
+        before { subject.phone_option = false }
+
+        it { expect(subject.valid?).to be true }
+      end
+
+      describe 'phone' do
+        context 'when phone_option is true' do
+          context 'and phone is given' do
+            it { expect(subject.valid?).to be true }
+          end
+
+          context 'and phone is not given' do
+            before { subject.phone = '' }
 
             it { expect(subject.valid?).to be false }
           end

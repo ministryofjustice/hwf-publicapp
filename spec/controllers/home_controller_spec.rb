84f9469cbd77b6dc8b57472bd1084d0b59260268
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2016-02-17 08:21:44 +0000

    Correction of the example name
    There was an incorrect name for one of the examples.
    But not any more!

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -217,7 +217,7 @@ RSpec.describe HomeController, type: :controller do
     end
   end
 
-  describe 'GET #address' do
+  describe 'GET #applicant_address' do
     it 'returns http success' do
       get :applicant_address
       expect(response).to have_http_status(:success)

ae7c12eb1440d4d1296fa7da91670f74414b43ff
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-02-19 10:47:52 +0000

    Updated national_insurance page
    * added error handling
    * moved text to summary and question.yam
    * feature tests

---
+++ spec/features/pages/national_insurance_question_spec.rb
@@ -0,0 +1,45 @@
+# coding: utf-8
+require 'rails_helper'
+
+RSpec.feature 'As a user' do
+  context 'when accessing the "national-insurance" page for "Help with fees"' do
+    before { page.visit 'national-insurance' }
+
+    context 'completing the form correctly' do
+      before do
+        fill_in :number, with: 'AB123456A'
+        click_button 'Continue'
+      end
+
+      scenario 'I expect to be routed to the "personal-detail" page' do
+        expect(page).to have_content 'What is your full name?'
+      end
+    end
+
+    context 'not completing the page correctly' do
+      describe 'leaving the field empty' do
+        before do
+          click_button 'Continue'
+        end
+
+        scenario 'I expect to be shown the "national-insurance" page with error block' do
+          expect(page).to have_content 'You need to fix the errors on this page before continuing.'
+        end
+
+        scenario 'I expect the fields to have specific errors' do
+          expect(page).to have_xpath('//span[@class="error-message"]', text: 'Enter your National Insurance number')
+        end
+      end
+      describe 'providing an incorrect value' do
+        before do
+          fill_in :number, with: 'AB123'
+          click_button 'Continue'
+        end
+
+        scenario 'I expect the fields to have specific errors' do
+          expect(page).to have_xpath('//span[@class="error-message"]', text: 'Enter a valid National Insurance number')
+        end
+      end
+    end
+  end
+end

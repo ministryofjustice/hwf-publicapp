b5d4572443aa0cb2a1416ebeb134106f19ffc97a
axemonkey <hi+github@clivemurray.com>
2016-02-23 15:49:23 +0000

    show/hide js rewrite

--- app/assets/javascripts/modules/selection-buttons.js
+++ app/assets/javascripts/modules/selection-buttons.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.moj.Modules.RadioButtonsModule = {
+window.moj.Modules.SelectionButtonsModule = {
   init: function() {
     var $buttons = $("label input[type='radio'], label input[type='checkbox']"),
         selectionButtons = new GOVUK.SelectionButtons($buttons);
--- app/assets/javascripts/modules/show-hide.js
+++ app/assets/javascripts/modules/show-hide.js
@@ -2,9 +2,69 @@
 
 window.moj.Modules.ShowHide = {
   init: function() {
-    var toggleContent = new ShowHideContent();
+    var self = this,
+        $elements = $(".block-label input[type='radio'], .block-label input[type='checkbox']");
 
-    toggleContent.showHideRadioToggledContent();
-    toggleContent.showHideCheckboxToggledContent();
+    if ($elements.length) {
+      self.initCheckElements($elements);
+      self.bindElementEvents($elements);
+    }
+  },
+
+  initCheckElements: function($elements) {
+    var self = this;
+
+    $elements.each(function(n, element) {
+      var $element = $(element);
+
+      if ($element.is(':checked')) {
+        self.toggleElementContent($element);
+      }
+    });
+  },
+
+  bindElementEvents: function($elements) {
+    var self = this;
+
+    $elements.on('click', function(e) {
+      self.toggleElementContent($(e.target));
+    });
+  },
+
+  toggleElementContent: function($element) {
+    var self = this,
+        target = $element.closest('label').data('target'),
+        show = $element.is(':checked');
+
+    if($element.attr('type') === 'radio') {
+      var radioGroup = $element.attr('name');
+
+      // hide any conditional content attached to radios in this group
+      $element.closest('form').find('.block-label input[name="' + radioGroup + '"]').each(function(n, groupMember) {
+          var $groupMember = $(groupMember),
+              memberTarget = $groupMember.closest('label').data('target'),
+              $memberTarget = $('#' + memberTarget);
+
+          $memberTarget.hide();
+          $memberTarget.attr('aria-hidden', 'true');
+          $groupMember.attr('aria-expanded', 'false');
+      });
+    }
+
+    if(target) {
+      self.showContent($element, target, show);
+    }
+  },
+
+  showContent: function($element, target, show) {
+    var $targetElement = $('#' + target);
+
+    $element.attr('aria-expanded', !!show);
+    $targetElement.attr('aria-hidden', !show);
+    if(show) {
+      $targetElement.show();
+    } else {
+      $targetElement.hide();
+    }
   }
 };

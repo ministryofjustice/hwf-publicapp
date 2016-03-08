daaca71400f45d0f1ac3d8a50d5feddff2668a5f
Leigh Money <Leigh.Money@digital.justice.gov.uk>
2016-03-08 10:37:38 +0000

    Fixing tests for update button copy

--- spec/features/user_details_are_not_persisted_spec.rb
+++ spec/features/user_details_are_not_persisted_spec.rb
@@ -59,7 +59,7 @@ RSpec.feature 'User details are not persisted' do
   end
 
   def when_they_submit_the_application
-    click_link 'Complete and send application'
+    click_link 'Complete application'
   end
 
   def when_they_go_back_to_homepage_and_start_again

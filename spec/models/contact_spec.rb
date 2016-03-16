2f82851e04c388277940c463ebf265a693c21e13
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-03-16 16:04:36 +0000

    Updated contact
    * removed unused options
    * added feedback opt in function

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,136 +2,75 @@ require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
   subject do
-    described_class.new(email_option: true,
-                        email: 'foo@bar.com',
-                        phone_option: true,
-                        phone: '078 112 123 22',
-                        post_option: true)
+    described_class.new(email: 'foo@bar.com',
+                        feedback_opt_in: true)
   end
 
   describe 'validations' do
-    describe 'email_option' do
-      context 'when true' do
+    describe 'email' do
+      context 'and valid email is given' do
         it { expect(subject.valid?).to be true }
       end
 
-      context 'when false' do
-        before { subject.email_option = false }
+      context 'and invalid email is given' do
+        before { subject.email = 'foobar.com' }
 
-        it { expect(subject.valid?).to be true }
+        it { expect(subject.valid?).to be false }
       end
 
-      describe 'email' do
-        context 'when email_option is true' do
-          context 'and email is given' do
-            it { expect(subject.valid?).to be true }
-          end
-
-          context 'and email is not given' do
-            before { subject.email = '' }
-
-            it { expect(subject.valid?).to be false }
-          end
-        end
-      end
-    end
-
-    describe 'phone_option' do
-      context 'when true' do
-        it { expect(subject.valid?).to be true }
-      end
-
-      context 'when false' do
-        before { subject.phone_option = false }
-
-        it { expect(subject.valid?).to be true }
-      end
-
-      describe 'phone' do
-        context 'when phone_option is true' do
-          context 'and phone is given' do
-            it { expect(subject.valid?).to be true }
-          end
-
-          context 'and phone is not given' do
-            before { subject.phone = '' }
-
-            it { expect(subject.valid?).to be false }
-          end
-        end
-      end
-    end
-
-    describe 'post_option' do
-      context 'when true' do
-        it { expect(subject.valid?).to be true }
-      end
-
-      context 'when false' do
-        before { subject.post_option = false }
+      context 'and email is not given' do
+        before { subject.email = '' }
 
         it { expect(subject.valid?).to be true }
       end
     end
 
-    describe 'when no option is picked' do
-      # TODO: discuss this
-      before do
-        subject.email_option = false
-        subject.phone_option = false
-        subject.post_option = false
+    describe 'feedback_opt_in' do
+      context 'when true' do
+        it { expect(subject.valid?).to be true }
+      end
+
+      context 'when false' do
+        before { subject.feedback_opt_in = false }
+
+        it { expect(subject.valid?).to be true }
       end
     end
   end
 
   describe '#export' do
-    let(:email_option) { true }
     let(:email) { 'some@email.domain' }
-    let(:phone_option) { true }
-    let(:phone) { '12345678' }
-    let(:post_option) { true }
+    let(:feedback_opt_in) { true }
 
-    subject { described_class.new(email_option: email_option, email: email, phone_option: phone_option, phone: phone, post_option: post_option).export }
+    subject { described_class.new(email: email, feedback_opt_in: feedback_opt_in).export }
 
-    context 'when email_option is true' do
+    context 'when email is set' do
       it 'the returned hash includes email_contact true and email_address' do
         is_expected.to include(email_contact: true, email_address: email)
       end
     end
 
-    context 'when email_option is false' do
-      let(:email_option) { false }
+    context 'when email is not set' do
+      let(:email) { nil }
 
-      it 'the returned hash includes email_contact true and email_address is nil' do
-        is_expected.to include(email_contact: false, email_address: nil)
+      it 'the returned hash includes email_contact false and email_address is nil' do
+        is_expected.to include(email_contact: false, email_address: email)
       end
     end
 
-    context 'when phone_option is true' do
-      it 'the returned hash includes phone_contact true and phone' do
-        is_expected.to include(phone_contact: true, phone: phone)
-      end
+    it 'the returned hash always returns phone_contact as false' do
+      is_expected.to include(phone_contact: false)
     end
 
-    context 'when phone_option is false' do
-      let(:phone_option) { false }
-
-      it 'the returned hash includes phone_contact true and phone is nil' do
-        is_expected.to include(phone_contact: false, phone: nil)
-      end
+    it 'the returned hash includesreturns post_contact as false' do
+      is_expected.to include(post_contact: false)
     end
 
-    context 'when post_option is true' do
-      it 'the returned hash includes post_contact true' do
-        is_expected.to include(post_contact: true)
-      end
-    end
+    context 'when feedback_opt_in is false' do
+      let(:feedback_opt_in) { false }
 
-    context 'when post_option is false' do
-      let(:post_option) { false }
-
-      it 'the returned hash includes post_contact true' do
-        is_expected.to include(post_contact: false)
+      it 'the returned hash includes feedback_opt_in false' do
+        is_expected.to include(feedback_opt_in: false)
       end
     end
   end

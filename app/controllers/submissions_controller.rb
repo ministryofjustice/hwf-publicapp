8f636e9ffbc34f083d1293c104c0553aacd2b5fb
Jakub Novotny <novotny.jakub@gmail.com>
2016-03-17 11:19:19 +0000

    Submit application from SubmissionsController

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,6 +1,15 @@
 class SubmissionsController < ApplicationController
   def create
-    redirect_to(action: :show)
+    builder = OnlineApplicationBuilder.new(session)
+    submit_service = SubmitApplication.new(Settings.submission.url, Settings.submission.token)
+    response = submit_service.post(builder.online_application)
+    session[:submission_response] = response
+
+    if response[:result]
+      redirect_to(action: :show)
+    else
+      redirect_to(summary_path)
+    end
   end
 
   def show
--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -3,12 +3,42 @@ require 'rails_helper'
 RSpec.describe SubmissionsController, type: :controller do
 
   describe 'POST #create' do
+    let(:session) { double(:[]= => true) }
+    let(:online_application) { double }
+    let(:builder) { double(online_application: online_application) }
+    let(:service) { double }
+
     before do
+      allow(controller).to receive(:session).and_return(session)
+      allow(OnlineApplicationBuilder).to receive(:new).with(session).and_return(builder)
+      allow(SubmitApplication).to receive(:new).and_return(service)
+      allow(service).to receive(:post).with(online_application).and_return(response)
+
       post :create
     end
 
-    it 'redirects to the show action' do
-      expect(response).to redirect_to(submission_path)
+    context 'on a successful response' do
+      let(:response) { { result: true, message: 'HWF-00000' } }
+
+      it 'stores the result on the session' do
+        expect(session).to have_received(:[]=).with(:submission_response, response)
+      end
+
+      it 'redirects to the show action' do
+        expect(response).to redirect_to(submission_path)
+      end
+    end
+
+    context 'on a failed response' do
+      let(:response) { { result: false, message: 'Failed' } }
+
+      it 'stores the result on the session' do
+        expect(session).to have_received(:[]=).with(:submission_response, response)
+      end
+
+      it 'redirects to the confirmation page' do
+        expect(response).to redirect_to(summary_path)
+      end
     end
   end
 

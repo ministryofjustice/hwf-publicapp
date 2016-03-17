8d7ee086480586ce26664619056a5147297035bd
Jakub Novotny <novotny.jakub@gmail.com>
2016-03-17 14:37:57 +0000

    Refactor the SubmissionController

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,19 +1,27 @@
 class SubmissionsController < ApplicationController
   def create
-    builder = OnlineApplicationBuilder.new(session)
-    submit_service = SubmitApplication.new(Settings.submission.url, Settings.submission.token)
     response = submit_service.post(builder.online_application)
     session[:submission_result] = response
 
-    if response[:result]
-      redirect_to(action: :show)
-    else
-      redirect_to(summary_path)
-    end
+    redirect_to(redirect_path(response))
   end
 
   def show
     @result = session[:submission_result]
     reset_session
+  end
+
+  private
+
+  def submit_service
+    @submit_service ||= SubmitApplication.new(Settings.submission.url, Settings.submission.token)
+  end
+
+  def builder
+    @builder ||= OnlineApplicationBuilder.new(session)
+  end
+
+  def redirect_path(response)
+    response[:result] ? { action: :show } : summary_path
   end
 end

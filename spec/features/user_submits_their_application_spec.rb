206e1dc32203427ff8802dd232f7281116ee6fa7
Jakub Novotny <novotny.jakub@gmail.com>
2016-03-29 17:10:58 +0100

    Add feature test for refund confirmation page

--- spec/features/user_submits_their_application_spec.rb
+++ spec/features/user_submits_their_application_spec.rb
@@ -3,11 +3,18 @@ require 'rails_helper'
 RSpec.feature 'User submits their application' do
   let(:reference) { 'HWF-CIDI16' }
 
-  scenario 'User submits their application and it is successfully processed' do
+  scenario 'User submits their non refund application and it is successfully processed' do
     given_user_provides_all_data
     and_the_submission_service_is_available(reference)
     when_they_submit_the_application
-    then_they_are_presented_with_the_confirmation_page_with_reference_number
+    then_they_are_presented_with_the_non_refund_confirmation_page_with_reference_number
+  end
+
+  scenario 'User submits their refund application and it is successfully processed' do
+    given_user_provides_all_data_for_refund
+    and_the_submission_service_is_available(reference)
+    when_they_submit_the_application
+    then_they_are_presented_with_the_refund_confirmation_page_with_reference_number
   end
 
   scenario 'User submits their application, but it is not processed' do
@@ -17,9 +24,17 @@ RSpec.feature 'User submits their application' do
     then_they_are_redirected_to_the_summary_page_with_error_message
   end
 
-  def then_they_are_presented_with_the_confirmation_page_with_reference_number
+  def then_they_are_presented_with_the_non_refund_confirmation_page_with_reference_number
     expect(page.current_path).to eql('/submission')
     expect(page).to have_content(reference)
+    expect(page).to have_content('Send your claim form to the court or tribunal dealing with your case')
+  end
+
+  def then_they_are_presented_with_the_refund_confirmation_page_with_reference_number
+    expect(page.current_path).to eql('/submission')
+    expect(page).to have_content('Send your reference number to the court or tribunal dealing with your case')
+    expect(page).to have_content(reference)
+    expect(page).to have_content('Sir Bob Oliver')
   end
 
   def then_they_are_redirected_to_the_summary_page_with_error_message
--- spec/support/feature_steps.rb
+++ spec/support/feature_steps.rb
@@ -35,6 +35,43 @@ module FeatureSteps
     click_button 'Continue'
   end
 
+  def given_user_provides_all_data_for_refund
+    visit '/'
+    click_link_or_button 'Apply now'
+    choose 'marital_status_married_false'
+    click_button 'Continue'
+    choose 'savings_and_investment_less_than_limit_false'
+    click_button 'Continue'
+    choose 'benefit_on_benefits_false'
+    click_button 'Continue'
+    choose 'dependent_children_false'
+    click_button 'Continue'
+    fill_in :income_wages, with: 100.0
+    click_button 'Continue'
+    choose 'fee_paid_true'
+    fill_in :fee_date_paid, with: 3.weeks.ago
+    click_button 'Continue'
+    choose 'probate_kase_false'
+    click_button 'Continue'
+    choose 'claim_number_false'
+    click_button 'Continue'
+    fill_in 'form_name_identifier', with: 'N1'
+    click_button 'Continue'
+    fill_in 'national_insurance_number', with: 'AB123456A'
+    click_button 'Continue'
+    fill_in 'dob_date_of_birth', with: '01/01/1980'
+    click_button 'Continue'
+    fill_in 'personal_detail_title', with: 'Sir'
+    fill_in 'personal_detail_first_name', with: 'Bob'
+    fill_in 'personal_detail_last_name', with: 'Oliver'
+    click_button 'Continue'
+    fill_in 'applicant_address_address', with: 'Foo Street'
+    fill_in 'applicant_address_postcode', with: 'Bar'
+    click_button 'Continue'
+    fill_in 'contact_email', with: 'foo@bar.com'
+    click_button 'Continue'
+  end
+
   def given_user_starts_an_application
     visit '/'
     click_link_or_button 'Apply now'

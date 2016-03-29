296933820f66c08634b8d0495f6bcbed7d3e0b6a
Jakub Novotny <novotny.jakub@gmail.com>
2016-03-29 17:22:38 +0100

    Consolidate feature steps helper

--- spec/support/feature_steps.rb
+++ spec/support/feature_steps.rb
@@ -1,82 +1,47 @@
+# rubocop:disable ModuleLength
 module FeatureSteps
   def given_user_provides_all_data
     visit '/'
     click_link_or_button 'Apply now'
-    choose 'marital_status_married_false'
-    click_button 'Continue'
-    choose 'savings_and_investment_less_than_limit_false'
-    click_button 'Continue'
-    choose 'benefit_on_benefits_false'
-    click_button 'Continue'
-    choose 'dependent_children_false'
-    click_button 'Continue'
-    fill_in :income_wages, with: 100.0
-    click_button 'Continue'
-    choose 'fee_paid_false'
-    click_button 'Continue'
-    choose 'probate_kase_false'
-    click_button 'Continue'
-    choose 'claim_number_false'
-    click_button 'Continue'
-    fill_in 'form_name_identifier', with: 'N1'
-    click_button 'Continue'
-    fill_in 'national_insurance_number', with: 'AB123456A'
-    click_button 'Continue'
-    fill_in 'dob_date_of_birth', with: '01/01/1980'
-    click_button 'Continue'
-    fill_in 'personal_detail_title', with: 'Sir'
-    fill_in 'personal_detail_first_name', with: 'Bob'
-    fill_in 'personal_detail_last_name', with: 'Oliver'
-    click_button 'Continue'
-    fill_in 'applicant_address_address', with: 'Foo Street'
-    fill_in 'applicant_address_postcode', with: 'Bar'
-    click_button 'Continue'
-    fill_in 'contact_email', with: 'foo@bar.com'
-    click_button 'Continue'
+    fill_marital_status
+    fill_savings_and_investment
+    fill_benefit
+    fill_dependent
+    fill_income
+    fill_fee
+    fill_probate
+    fill_claim
+    fill_form_name
+    fill_national_insurance
+    fill_dob
+    fill_personal_detail
+    fill_applicant_address
+    fill_contact
   end
 
   def given_user_provides_all_data_for_refund
     visit '/'
     click_link_or_button 'Apply now'
-    choose 'marital_status_married_false'
-    click_button 'Continue'
-    choose 'savings_and_investment_less_than_limit_false'
-    click_button 'Continue'
-    choose 'benefit_on_benefits_false'
-    click_button 'Continue'
-    choose 'dependent_children_false'
-    click_button 'Continue'
-    fill_in :income_wages, with: 100.0
-    click_button 'Continue'
-    choose 'fee_paid_true'
-    fill_in :fee_date_paid, with: 3.weeks.ago
-    click_button 'Continue'
-    choose 'probate_kase_false'
-    click_button 'Continue'
-    choose 'claim_number_false'
-    click_button 'Continue'
-    fill_in 'form_name_identifier', with: 'N1'
-    click_button 'Continue'
-    fill_in 'national_insurance_number', with: 'AB123456A'
-    click_button 'Continue'
-    fill_in 'dob_date_of_birth', with: '01/01/1980'
-    click_button 'Continue'
-    fill_in 'personal_detail_title', with: 'Sir'
-    fill_in 'personal_detail_first_name', with: 'Bob'
-    fill_in 'personal_detail_last_name', with: 'Oliver'
-    click_button 'Continue'
-    fill_in 'applicant_address_address', with: 'Foo Street'
-    fill_in 'applicant_address_postcode', with: 'Bar'
-    click_button 'Continue'
-    fill_in 'contact_email', with: 'foo@bar.com'
-    click_button 'Continue'
+    fill_marital_status
+    fill_savings_and_investment
+    fill_benefit
+    fill_dependent
+    fill_income
+    fill_fee(true)
+    fill_probate
+    fill_claim
+    fill_form_name
+    fill_national_insurance
+    fill_dob
+    fill_personal_detail
+    fill_applicant_address
+    fill_contact
   end
 
   def given_user_starts_an_application
     visit '/'
     click_link_or_button 'Apply now'
-    choose 'marital_status_married_false'
-    click_button 'Continue'
+    fill_marital_status
   end
 
   def when_they_submit_the_application
@@ -92,6 +57,85 @@ module FeatureSteps
     visit question_path(:marital_status)
     expect(page).to have_unchecked_field('marital_status_married_false')
     expect(page).to have_unchecked_field('marital_status_married_true')
+  end
+
+  def fill_contact
+    fill_in 'contact_email', with: 'foo@bar.com'
+    click_button 'Continue'
+  end
+
+  def fill_applicant_address
+    fill_in 'applicant_address_address', with: 'Foo Street'
+    fill_in 'applicant_address_postcode', with: 'Bar'
+    click_button 'Continue'
+  end
+
+  def fill_personal_detail
+    fill_in 'personal_detail_title', with: 'Sir'
+    fill_in 'personal_detail_first_name', with: 'Bob'
+    fill_in 'personal_detail_last_name', with: 'Oliver'
+    click_button 'Continue'
+  end
+
+  def fill_dob
+    fill_in 'dob_date_of_birth', with: '01/01/1980'
+    click_button 'Continue'
+  end
+
+  def fill_national_insurance
+    fill_in 'national_insurance_number', with: 'AB123456A'
+    click_button 'Continue'
+  end
+
+  def fill_form_name
+    fill_in 'form_name_identifier', with: 'N1'
+    click_button 'Continue'
+  end
+
+  def fill_claim
+    choose 'claim_number_false'
+    click_button 'Continue'
+  end
+
+  def fill_probate
+    choose 'probate_kase_false'
+    click_button 'Continue'
+  end
+
+  def fill_fee(refund = false)
+    if refund
+      choose 'fee_paid_true'
+      fill_in :fee_date_paid, with: 3.weeks.ago
+    else
+      choose 'fee_paid_false'
+    end
+
+    click_button 'Continue'
+  end
+
+  def fill_income
+    fill_in :income_wages, with: 100.0
+    click_button 'Continue'
+  end
+
+  def fill_dependent
+    choose 'dependent_children_false'
+    click_button 'Continue'
+  end
+
+  def fill_benefit
+    choose 'benefit_on_benefits_false'
+    click_button 'Continue'
+  end
+
+  def fill_savings_and_investment
+    choose 'savings_and_investment_less_than_limit_false'
+    click_button 'Continue'
+  end
+
+  def fill_marital_status
+    choose 'marital_status_married_false'
+    click_button 'Continue'
   end
 end
 

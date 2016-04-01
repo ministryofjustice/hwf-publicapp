35d0aeb94af4a7143269d6f7ed431ccaa63de678
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-04-01 15:35:36 +0100

    re-worded tests

--- spec/features/pages/summary_spec.rb
+++ spec/features/pages/summary_spec.rb
@@ -41,7 +41,7 @@ RSpec.feature 'As a user' do
       end
 
       scenario 'I expect to see my answers' do
-        expect(page).not_to have_content 'Probate case'
+        expect(page).to have_no_content 'Probate case'
         expect(page).to have_content 'Name of deceasedFooChange'
         expect(page).to have_content "Date of death#{(Time.zone.today - 1.month).strftime(Date::DATE_FORMATS[:default])}Change"
       end
@@ -57,8 +57,8 @@ RSpec.feature 'As a user' do
 
       scenario 'I do not expect to see the probate sub headers' do
         expect(page).to have_content 'Probate case'
-        expect(page).not_to have_content 'Name of deceased'
-        expect(page).not_to have_content 'Date of death'
+        expect(page).to have_no_content 'Name of deceased'
+        expect(page).to have_no_content 'Date of death'
       end
     end
 

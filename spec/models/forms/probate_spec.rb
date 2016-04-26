b6f471413214f218075088a227c41636e0c9fd04
Jakub Novotny <novotny.jakub@gmail.com>
2016-04-26 17:37:18 +0100

    Validate deceased_name to be max 100 chars

--- spec/models/forms/probate_spec.rb
+++ spec/models/forms/probate_spec.rb
@@ -1,82 +1,74 @@
 require 'rails_helper'
 
 RSpec.describe Forms::Probate, type: :model do
-  subject { described_class.new }
+  let(:params) { { kase: kase, deceased_name: deceased_name, date_of_death: date_of_death } }
+  subject(:form) { described_class.new(params) }
 
   describe 'validations' do
+    let(:deceased_name) { nil }
+    let(:date_of_death) { nil }
+
     describe 'kase' do
       context 'when false' do
-        before { subject.kase = false }
+        let(:kase) { false }
 
-        it { expect(subject.valid?).to be true }
+        it { is_expected.to be_valid }
+      end
 
-        context 'when "deceased_name" is not set' do
-          before { subject.deceased_name = nil }
+      context 'when true' do
+        let(:kase) { true }
+        let(:deceased_name) { 'foo' }
+        let(:date_of_death) { Time.zone.today - 1.month }
 
-          it { expect(subject.valid?).to be true }
+        describe 'date of death' do
+          describe 'when missing' do
+            let(:date_of_death) { nil }
+
+            it { is_expected.not_to be_valid }
+          end
+
+          describe 'range' do
+            context 'exceeds upper bound' do
+              let(:date_of_death) { Time.zone.tomorrow }
+
+              it { is_expected.not_to be_valid }
+            end
+
+            context 'exceeds lower bound' do
+              let(:date_of_death) { Time.zone.today - 21.years }
+
+              it { is_expected.not_to be_valid }
+            end
+          end
         end
 
-        context 'when "date_of_death" is not set' do
-          before { subject.date_of_death = nil }
+        describe 'deceased_name' do
+          context 'when present' do
+            context 'when more than 100 characters long' do
+              let(:deceased_name) { 'X' * 101 }
 
-          it { expect(subject.valid?).to be true }
+              it { is_expected.not_to be_valid }
+            end
+
+            context 'when less than 100 characters long' do
+              let(:deceased_name) { 'X' * 100 }
+
+              it { is_expected.to be_valid }
+            end
+          end
+
+          context 'when not set' do
+            let(:deceased_name) { nil }
+
+            it { is_expected.not_to be_valid }
+          end
         end
       end
 
       context 'when not a boolean value' do
-        before { subject.kase = 'string' }
+        let(:kase) { 'string' }
 
-        it { expect(subject.valid?).to be false }
-      end
-    end
-
-    context 'when "kase" is true' do
-      before do
-        subject.kase = true
-        # also set default 'good' values
-        subject.date_of_death = Time.zone.today - 1.month
-        subject.deceased_name = 'foo'
-      end
-
-      describe 'date of death' do
-        describe 'is missing' do
-          before { subject.date_of_death = nil }
-
-          it { expect(subject.valid?).to be false }
-
-          context 'it returns an error' do
-            before { subject.valid? }
-
-            it { expect(subject.errors[:date_of_death]).to eq ['Enter the date of death'] }
-          end
-        end
-
-        describe 'range' do
-          context 'exceeds upper bound' do
-            before { subject.date_of_death = Time.zone.tomorrow }
-
-            it { expect(subject.valid?).to be false }
-          end
-
-          context 'exceeds lower bound' do
-            before { subject.date_of_death = Time.zone.today - 21.years }
-
-            it { expect(subject.valid?).to be false }
-          end
-        end
-      end
-
-      describe 'deceased_name' do
-        context 'when "deceased_name" is present' do
-
-          it { expect(subject.valid?).to be true }
-        end
-
-        context 'when "deceased_name" is not set' do
-          before { subject.deceased_name = nil }
-
-          it { expect(subject.valid?).to be false }
-        end
+        it { is_expected.not_to be_valid }
       end
     end
   end
@@ -85,7 +77,7 @@ RSpec.describe Forms::Probate, type: :model do
     let(:deceased_name) { 'Mr. Deceased' }
     let(:date_of_death) { Date.parse('01/01/2016') }
 
-    subject { described_class.new(kase: kase, deceased_name: deceased_name, date_of_death: date_of_death).export }
+    subject { form.export }
 
     context 'when kase is true' do
       let(:kase) { true }

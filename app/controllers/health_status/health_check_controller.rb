881318d7288aab79ee28fc2a2da9b8d94ca33043
Jakub Novotny <novotny.jakub@gmail.com>
2016-04-28 12:46:45 +0100

    Check submission api in healthcheck

--- app/controllers/health_status/health_check_controller.rb
+++ app/controllers/health_status/health_check_controller.rb
@@ -2,7 +2,7 @@ module HealthStatus
   class HealthCheckController < ApplicationController
     def show
       health_check = HealthStatus::HealthCheck.new
-      render json: health_check, status: health_check.status ? 200 : 500
+      render json: health_check, status: health_check.healthy? ? 200 : 500
     end
   end
 end
--- app/services/health_status/health_check.rb
+++ app/services/health_status/health_check.rb
@@ -1,20 +1,28 @@
 module HealthStatus
   class HealthCheck
     def initialize
+      check_submission_api!
     end
 
     def as_json(_options = {})
       {
         submission_api: {
           description: 'Submission API',
-          ok: true
+          ok: @submission_api_available
         },
-        ok: status
+        ok: @submission_api_available
       }
     end
 
-    def status
-      true
+    def healthy?
+      @submission_api_available
+    end
+
+    private
+
+    def check_submission_api!
+      service = SubmitApplication.new(Settings.submission.url, Settings.submission.token)
+      @submission_api_available = service.available?
     end
   end
 end

dc527b8bfceeaa07b65d1abc1d262533fd4cc94c
Jakub Novotny <novotny.jakub@gmail.com>
2016-05-11 23:49:00 +0100

    Create HelpRequest form model

---
+++ app/models/forms/help_request.rb
@@ -0,0 +1,9 @@
+module Forms
+  class HelpRequest < Base
+    attribute :name, String
+    attribute :phone, String
+    attribute :description, String
+
+    validates :name, :phone, :description, presence: true
+  end
+end

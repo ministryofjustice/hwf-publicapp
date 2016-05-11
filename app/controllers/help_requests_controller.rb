1a81100feb2e614e552c8c6b7a4fad6e4d9c2096
Jakub Novotny <novotny.jakub@gmail.com>
2016-05-12 00:10:19 +0100

    Prepare the help request pages
    The next bit is to plug in Zendesk and send the data to it.

---
+++ app/controllers/help_requests_controller.rb
@@ -0,0 +1,26 @@
+class HelpRequestsController < ApplicationController
+  def new
+    form
+  end
+
+  def create
+    form.update_attributes(help_request_params)
+
+    if form.valid?
+      flash[:info] = I18n.t('help_requests.success')
+      redirect_to(root_path)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def form
+    @form ||= Forms::HelpRequest.new
+  end
+
+  def help_request_params
+    params.require(form.id).permit(*form.permitted_attributes)
+  end
+end

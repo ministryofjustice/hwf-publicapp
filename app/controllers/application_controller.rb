86af05a97b29f0777781b8fa4ea4f3fb0eeb3a00
Jakub Novotny <novotny.jakub@gmail.com>
2016-05-26 17:11:45 +0200

    Display error and redirect on session expiry
    The solution is very basic and there will be subsequent stories to deal
    with proper expiration and prompt to extend the session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from Storage::Expired, with: :expired_redirect
+
   private
 
   def online_application
@@ -25,6 +27,11 @@ class ApplicationController < ActionController::Base
 
   def redirect_if_storage_unstarted
     redirect_to(root_path) unless storage.started?
+  end
+
+  def expired_redirect
+    flash[:error] = t('session.expired_message')
+    redirect_to(root_path)
   end
 
   def ga_events

7716573d1e42ad3e96a457e822ad825a2e8d673a
Clive Murray <hi+github@clivemurray.com>
2016-06-10 15:08:55 +0100

    if we're opening new windows, warn people

--- app/assets/javascripts/modules/external-links.js
+++ app/assets/javascripts/modules/external-links.js
@@ -2,6 +2,25 @@
 
 window.moj.Modules.ExternalLinks = {
   init: function() {
-    $('a[rel="external"]').attr('target', '_blank');
+    var self = this;
+
+    self.setupExternalLinks($('a[rel~="external"]'));
+  },
+
+  setupExternalLinks: function(obj) {
+    obj.each(function(n, el) {
+      var $el = $(el),
+          winStr = ' (Opens in a new window/tab)',
+          currRel = $el.attr('rel'),
+          newRel = currRel + ' noopener',
+          currTitle = $el.attr('title') ? $el.attr('title') : $el.text(),
+          newTitle = currTitle + winStr;
+
+      $el.attr({
+        'rel': newRel,
+        'target': '_blank',
+        'title': newTitle
+      }).html($el.html() + '<span class="visuallyhidden">' + winStr + '</span>');
+    });
   }
 };

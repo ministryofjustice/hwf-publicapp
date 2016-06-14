4fb06a2ed07bb7823aa1a520d5778720e55ebb37
Jakub Novotny <novotny.jakub@gmail.com>
2016-06-14 08:27:33 +0100

    Add feature test for the savings extra page

---
+++ spec/features/pages/savings_investment_extra_question_spec.rb
@@ -0,0 +1,38 @@
+# coding: utf-8
+require 'rails_helper'
+
+RSpec.feature 'As a user' do
+  context 'when accessing the "savings-and-investment extra" page for "Help with fees"' do
+    before do
+      given_user_answers_questions_up_to(:savings_and_investment)
+      choose 'savings_and_investment_choice_between'
+      click_button 'Continue'
+    end
+
+    context 'completing the form correctly' do
+      before do
+        choose 'savings_and_investment_extra_over_61_false'
+        fill_in 'savings_and_investment_extra_amount', with: 6000
+        click_button 'Continue'
+      end
+
+      scenario 'I expect to be routed to the "benefits" page' do
+        expect(page).to have_content 'Do you receive any of the following benefits?'
+      end
+    end
+
+    context 'not completing the page correctly' do
+      before do
+        click_button 'Continue'
+      end
+
+      scenario 'I expect to be shown the "savings-and-investment extra" page with error block' do
+        expect(page).to have_content 'You need to fix the errors on this page before continuing.'
+      end
+
+      scenario 'I expect the fields to have specific errors' do
+        expect(page).to have_xpath('//span[@class="error-message"]', text: 'Select if you or your partner are over 61')
+      end
+    end
+  end
+end

5b289bda387c1fef856c732e3b8e799e156e3f53
Jakub Novotny <novotny.jakub@gmail.com>
2016-07-07 11:34:53 +0100

    Use view model to render list of income kinds

---
+++ app/models/views/income_kinds.rb
@@ -0,0 +1,27 @@
+module Views
+  class IncomeKinds
+    def initialize(session)
+      @session = session
+    end
+
+    def list
+      kinds.map do |kind|
+        I18n.t(kind, scope: ['questions.income_kind.kinds'])
+      end
+    end
+
+    private
+
+    def kinds
+      (source.fetch('applicant', []) + source.fetch('partner', [])).tap do |arr|
+        arr.uniq!
+        arr.sort!
+        arr.delete_if { |kind| kind == Forms::IncomeKind.no_income_index }
+      end
+    end
+
+    def source
+      @session.fetch('questions', {}).fetch('income_kind', {})
+    end
+  end
+end
---
+++ spec/models/views/income_kinds_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe Views::IncomeKinds do
+  subject(:view) { described_class.new(session) }
+
+  describe '#list' do
+    let(:session) { { 'questions' => { 'income_kind' => income_kind } } }
+    subject { view.list }
+
+    context 'when only applicant kinds are present' do
+      let(:income_kind) { { 'applicant' => applicant } }
+
+      context 'when they contain "no income" and others' do
+        let(:applicant) { [1, 8, 13] }
+
+        it 'returns list of translated kinds without "no income"' do
+          is_expected.to eql(['Wages', 'Universal Credit'])
+        end
+      end
+
+      context 'when they do not contain "no income"' do
+        let(:applicant) { [1, 8] }
+
+        it 'returns list of translated kinds' do
+          is_expected.to eql(['Wages', 'Universal Credit'])
+        end
+      end
+    end
+
+    context 'when applicant and partner kinds are present' do
+      let(:income_kind) { { 'applicant' => applicant, 'partner' => partner } }
+
+      context 'when they contain "no income" and others' do
+        let(:applicant) { [1, 8, 13] }
+        let(:partner) { [1, 5, 13] }
+
+        it 'returns list of merged translated kinds without "no income"' do
+          is_expected.to eql(['Wages', 'Maintenance payments', 'Universal Credit'])
+        end
+      end
+
+      context 'when they do not contain "no income"' do
+        let(:applicant) { [1, 8] }
+        let(:partner) { [1, 5] }
+
+        it 'returns list of merged translated kinds' do
+          is_expected.to eql(['Wages', 'Maintenance payments', 'Universal Credit'])
+        end
+      end
+    end
+
+    context 'when no kinds are present' do
+      let(:income_kind) { {} }
+
+      it 'returns an empty array' do
+        is_expected.to be_empty
+      end
+    end
+  end
+end

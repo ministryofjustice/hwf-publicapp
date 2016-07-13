1e510cb0941b3a58e64e36ea13efa88144f80d3e
Jakub Novotny <novotny.jakub@gmail.com>
2016-07-13 13:01:31 +0100

    Add refund copy for all income pages

--- app/models/views/question_title.rb
+++ app/models/views/question_title.rb
@@ -7,18 +7,19 @@ module Views
 
     def i18n_title
       'text'.tap do |i18n|
-        if %w[benefit income].include?(@form.id)
-          i18n << '_refund' if @online_application.refund?
-        elsif status_questions.include?(@form.id)
+        if (%w[savings_and_investment_extra] + income_questions).include?(@form.id)
           i18n << (@online_application.married? ? '_married' : '_single')
+        end
+        if (%w[benefit] + income_questions).include?(@form.id)
+          i18n << '_refund' if @online_application.refund?
         end
       end
     end
 
     private
 
-    def status_questions
-      %w[savings_and_investment_extra income_kind income_range income_amount]
+    def income_questions
+      %w[income_kind income_range income_amount]
     end
   end
 end
--- spec/models/views/question_title_spec.rb
+++ spec/models/views/question_title_spec.rb
@@ -9,44 +9,74 @@ RSpec.describe Views::QuestionTitle do
 
     subject { service.i18n_title }
 
-    %w[benefit income].each do |question|
-      context "for the #{question} question" do
-        let(:id) { question }
+    context 'for the benefit question' do
+      let(:id) { 'benefit' }
 
-        context 'when the online_application says it is a refund' do
-          let(:online_application) { build :online_application, :refund }
+      context 'when the online_application says it is a refund' do
+        let(:online_application) { build :online_application, :refund }
 
-          it { is_expected.to eql('text_refund') }
-        end
+        it { is_expected.to eql('text_refund') }
+      end
 
-        context 'when the online_application says it is not a refund' do
-          let(:online_application) { build :online_application }
+      context 'when the online_application says it is not a refund' do
+        let(:online_application) { build :online_application }
 
-          it { is_expected.to eql('text') }
-        end
+        it { is_expected.to eql('text') }
       end
     end
 
-    %w[savings_and_investment_extra income_kind income_range income_amount].each do |question|
+    context 'for the savings_and_investment_extra question' do
+      let(:id) { 'savings_and_investment_extra' }
+
+      context 'when the online_application says the user is married' do
+        let(:online_application) { build(:online_application, married: true) }
+
+        it { is_expected.to eql('text_married') }
+      end
+
+      context 'when the online_application says the user is single' do
+        let(:online_application) { build(:online_application, married: false) }
+
+        it { is_expected.to eql('text_single') }
+      end
+    end
+
+    %w[income_kind income_range income_amount].each do |question|
       context "for the #{question} question" do
         let(:id) { question }
 
         context 'when the online_application says the user is married' do
-          let(:online_application) { build(:online_application, married: true) }
+          context 'when the online_application says it is a refund' do
+            let(:online_application) { build(:online_application, :refund, married: true) }
 
-          it { is_expected.to eql('text_married') }
+            it { is_expected.to eql('text_married_refund') }
+          end
+
+          context 'when the online_application says it is not a refund' do
+            let(:online_application) { build(:online_application, married: true) }
+
+            it { is_expected.to eql('text_married') }
+          end
         end
 
         context 'when the online_application says the user is single' do
-          let(:online_application) { build(:online_application, married: false) }
+          context 'when the online_application says it is a refund' do
+            let(:online_application) { build(:online_application, :refund, married: false) }
 
-          it { is_expected.to eql('text_single') }
+            it { is_expected.to eql('text_single_refund') }
+          end
+
+          context 'when the online_application says it is not a refund' do
+            let(:online_application) { build(:online_application, married: false) }
+
+            it { is_expected.to eql('text_single') }
+          end
         end
       end
     end
 
     context 'for other questions' do
-      let(:id) { 'benefit' }
+      let(:id) { 'probate' }
 
       it { is_expected.to eql('text') }
     end

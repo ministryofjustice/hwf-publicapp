800b96b833b75803af7996acf89694bd9d7344a6
Jakub Novotny <novotny.jakub@gmail.com>
2016-07-20 08:56:26 +0100

    Export thresholds from the IncomeRange form

--- app/models/forms/income_range.rb
+++ app/models/forms/income_range.rb
@@ -7,7 +7,16 @@ module Forms
     private
 
     def export_params
-      {}
+      case choice
+      when :less
+        { income_min_threshold_exceeded: false }
+      when :between
+        { income_min_threshold_exceeded: true, income_max_threshold_exceeded: false }
+      when :more
+        { income_min_threshold_exceeded: true, income_max_threshold_exceeded: true }
+      else
+        {}
+      end
     end
   end
 end
--- spec/models/forms/income_range_spec.rb
+++ spec/models/forms/income_range_spec.rb
@@ -41,8 +41,36 @@ RSpec.describe Forms::IncomeRange, type: :model do
     let(:choice) { 'more' }
     subject { form.export }
 
-    it 'returns an empty hash' do
-      is_expected.to eql({})
+    context 'when "less"' do
+      let(:choice) { 'less' }
+
+      it 'returns a hash with income_min_threshold_exceeded set to false' do
+        is_expected.to eql(income_min_threshold_exceeded: false)
+      end
+    end
+
+    context 'when "between"' do
+      let(:choice) { 'between' }
+
+      it 'returns hash with income_min_threshold_exceeded parameter true and income_max_threshold_exceeded parameter false' do
+        is_expected.to eql(income_min_threshold_exceeded: true, income_max_threshold_exceeded: false)
+      end
+    end
+
+    context 'when "more"' do
+      let(:choice) { 'more' }
+
+      it 'returns hash with income_min_threshold_exceeded parameter true and income_max_threshold_exceeded parameter true' do
+        is_expected.to eql(income_min_threshold_exceeded: true, income_max_threshold_exceeded: true)
+      end
+    end
+
+    context 'when choice is not set' do
+      let(:choice) { nil }
+
+      it 'returns an empty hash' do
+        is_expected.to eql({})
+      end
     end
   end
 end

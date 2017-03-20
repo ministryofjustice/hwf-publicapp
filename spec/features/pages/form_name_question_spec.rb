d325c2dc7bd43ef018a7bd5d71db1d2ef32cc2db
Leonidas Apostolidis <leoapost@gmail.com>
2017-03-20 17:45:06 +0000

    create feature test for probate fees option

--- spec/features/pages/form_name_question_spec.rb
+++ spec/features/pages/form_name_question_spec.rb
@@ -24,5 +24,22 @@ RSpec.feature 'As a user' do
         expect(page).to have_content 'Enter the form name or number, or select \'I don’t know the form name or number\''
       end
     end
+
+    context 'choosing the probate fees option' do
+      before do
+        Capybara.current_driver = :webkit
+        Timecop.freeze(probate_fees_release_date) do
+          given_user_answers_questions_up_to(:form_name)
+        end
+      end
+
+      after { Timecop.return }
+
+      scenario 'I expect the submit button to be disabled' do
+        check 'form_name_probate'
+
+        expect(page).to have_css("input.button[name='commit'][disabled='disabled']")
+      end
+    end
   end
 end

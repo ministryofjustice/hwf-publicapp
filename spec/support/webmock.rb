9b48ba2cac96bf6e52fd4e53904a6bcb6b731462
Leonidas Apostolidis <leoapost@gmail.com>
2017-03-20 16:42:48 +0000

    whitelist 127.0.0.1 in webmock

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -2,6 +2,6 @@ require 'webmock/rspec'
 
 RSpec.configure do |config|
   config.before(:all) do
-    WebMock.disable_net_connect!(allow: %w[codeclimate.com])
+    WebMock.disable_net_connect!(allow: %w[codeclimate.com 127.0.0.1])
   end
 end

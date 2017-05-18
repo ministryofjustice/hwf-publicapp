f7531d91d1cb08a04f83857acc2b36e6e16e6fd5
Leonidas Apostolidis <leoapost@gmail.com>
2017-05-18 13:15:04 +0100

    fix Rubocop offenses

--- features/step_definitions/step_one_steps.rb
+++ features/step_definitions/step_one_steps.rb
@@ -16,9 +16,7 @@ When(/^I enter a valid form name$/) do
 end
 
 When(/^I select I don’t know the form name or number$/) do
-  form_name = step_one_page.content.new_form_name
-  expect(form_name.form_group[1].block_label[0].text)
-    .to eq 'I don’t know the form name or number'
+  form_name = step_one_page.content.new_form_name eq 'I don’t know the form name or number'
   expect(form_name.form_group[1].form_name_unknown['type']).to eq 'checkbox'
   form_name.form_group[1].form_name_unknown.click
   form_name.form_group[4].continue_button.click
@@ -26,10 +24,12 @@ end
 
 When(/^I select I need help with an employment tribunal fee$/) do
   form_name = step_one_page.content.new_form_name
-  expect(form_name.form_group[2].block_label[0].text)
-    .to eq 'I need help with an employment tribunal fee'
-  expect(form_name.form_group[2].form_name_et['type']).to eq 'checkbox'
-  form_name.form_group[2].form_name_et.click
+  form_group_two = form_name.form_group[2]
+  form_group_two_label = form_group_two.block_label[0].text
+
+  expect(form_group_two_label).to eq 'I need help with an employment tribunal fee'
+  expect(form_group_two.form_name_et['type']).to eq 'checkbox'
+  form_group_two.form_name_et.click
   form_name.form_group[4].continue_button.click
 end
 
@@ -53,8 +53,7 @@ end
 When(/^I see the are you sure copy$/) do
   confirm = step_one_page.restart_confirm
   expect(confirm.p.count).to eq 2
-  expect(confirm.p[0].text)
-    .to eq 'Are you sure you want to cancel your application?'
+  expect(confirm.p[0].text).to eq 'Are you sure you want to cancel your application?'
 end
 
 When(/^I click on no, return to current application$/) do
@@ -78,13 +77,12 @@ Then(/^I should see step number$/) do
 end
 
 Then(/^I should see the step one header$/) do
-  expect(step_one_page.content.page_header.text)
-    .to eq 'What court or tribunal fee do you need help with?'
+  page_header = step_one_page.content.page_header.text
+  expect(page_header).to eq 'What court or tribunal fee do you need help with?'
 end
 
 Then(/^I should see step one hint$/) do
-  expect(step_one_page.content.hint.text)
-    .to have_content 'Enter the court or tribunal form number'
+  expect(step_one_page.content.hint.text).to have_content 'Enter the court or tribunal form number'
 end
 
 Then(/^I should see step one label$/) do
@@ -94,13 +92,12 @@ end
 
 Then(/^I see error message telling me I need to fix the errors$/) do
   error_summary = step_one_page.content.error_summary
+  error_message = step_one_page.content.new_form_name.form_group[0].error_message.text
   expect(error_summary.h2.text).to have_content 'You need to fix the errors'
   expect(error_summary).to have_visuallyhidden
   expect(error_summary.error_link['href']).to end_with '#identifier'
-  expect(error_summary.error_link.text)
-    .to have_content 'Enter the form name or number'
-  expect(step_one_page.content.new_form_name.form_group[0].error_message.text)
-    .to have_content 'Enter the form name or number'
+  expect(error_summary.error_link.text).to have_content 'Enter the form name or number'
+  expect(error_message).to have_content 'Enter the form name or number'
 end
 
 Then(/^I see more information about the form name or number$/) do
@@ -115,13 +112,10 @@ end
 
 Then(/^I am taken to the Help with Fees homepage$/) do
   expect(current_path).to eq '/get-help-with-court-fees'
-  expect(step_one_page.content.page_header.text)
-    .to eq 'Get help paying court and tribunal fees'
+  expect(step_one_page.content.page_header.text).to eq 'Get help paying court and tribunal fees'
 end
 
 Then(/^I should not see the cancel application options$/) do
-  expect(step_one_page.restart_confirm.button[0])
-    .to_not be_visible
-  expect(step_one_page.restart_confirm.button[1])
-    .to_not be_visible
+  expect(step_one_page.restart_confirm.button[0]).to_not be_visible
+  expect(step_one_page.restart_confirm.button[1]).to_not be_visible
 end

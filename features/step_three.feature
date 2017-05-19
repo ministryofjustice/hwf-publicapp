9478492d66b9b2d815c3b156b06e767966e272b8
Alexa <alexaballantine@Alexas-MacBook-Pro.local>
2017-05-19 16:38:44 +0100

    RST-299-added step three tests

---
+++ features/step_three.feature
@@ -0,0 +1,22 @@
+Feature: Step three page
+
+  Background: Navigating to the page for step three
+    Given I visit the page for step three
+
+  Scenario: Displays step number
+    Then I should see step '3' of 20
+
+  Scenario: Displays header
+    Then I should see the step three header
+
+  Scenario: Selecting single
+    When I select single
+    Then I am taken to step four
+
+  Scenario: Selecting married
+    When I select married
+    Then I am taken to step four
+
+  Scenario: Help with status
+    When I click on help with status
+    Then I should see help with status copy
---
+++ features/support/page_objects/step_three_page.rb
@@ -0,0 +1,23 @@
+class StepThreePage < BasePage
+  section :content, '#content' do
+    element :p, 'p'
+    element :page_header, 'h1'
+    sections :form_group, '.form-group' do
+      elements :block_label, '.block-label'
+      element :marital_status_married_false, '#marital_status_married_false'
+      element :marital_status_married_true, '#marital_status_married_true'
+      element :continue_button, '.button'
+      element :help_with_status_button, 'details > summary'
+      section :details_content, '#details-content-0' do
+        elements :heading_small, '.heading-small'
+        sections :ul, 'ul' do
+          elements :li, 'li'
+        end
+      end
+    end
+  end
+
+  def load_page(page_version = nil)
+    load(v: page_version)
+  end
+end

91da679f5452c288d4f0a5d3dbbd69f3fa6d1d3d
Alexa <alexaballantine@Alexas-MacBook-Pro.local>
2017-06-01 16:36:45 +0100

    step three refactor

--- features/step_definitions/step_three_steps.rb
+++ features/step_definitions/step_three_steps.rb
@@ -1,3 +1,7 @@
+def form_group_3(index)
+  step_three_page.content.form_group[index]
+end
+
 Given(/^I visit the page for step three$/) do
   step 'I visit the page for step one'
   step 'I enter a valid form number'
@@ -5,35 +9,29 @@ Given(/^I visit the page for step three$/) do
 end
 
 When(/^I select single$/) do
-  from_group_one = step_three_page.content.form_group[0]
-
-  expect(from_group_one.block_label[0].text).to eq 'Single'
-  expect(from_group_one.marital_status_married_false['type']).to eq 'radio'
-  from_group_one.marital_status_married_false.click
-  step_three_page.content.form_group[2].continue_button.click
+  expect(form_group_3(0).block_label[0].text).to eq 'Single'
+  expect(form_group_3(0).marital_status_married_false['type']).to eq 'radio'
+  form_group_3(0).marital_status_married_false.click
+  form_group_3(2).continue_button.click
 end
 
 When(/^I select married$/) do
-  from_group_one = step_three_page.content.form_group[0]
-
-  expect(from_group_one.block_label[1].text).to have_content 'Married'
-  expect(from_group_one.marital_status_married_true['type']).to eq 'radio'
-  from_group_one.marital_status_married_true.click
-  step_three_page.content.form_group[2].continue_button.click
+  expect(form_group_3(0).block_label[1].text).to have_content 'Married'
+  expect(form_group_3(0).marital_status_married_true['type']).to eq 'radio'
+  form_group_3(0).marital_status_married_true.click
+  form_group_3(2).continue_button.click
 end
 
 Then(/^I should see step three label$/) do
-  form_group = step_three_page.content.new_form_name.form_group[0]
-  expect(form_group.label.text).to eq 'Form name or number'
+  expect(form_group_3(0).label.text).to eq 'Form name or number'
 end
 
 Then(/^I should see help with status copy$/) do
-  help_with_status_group = step_three_page.content.form_group[1].details_content
-  first_heading = help_with_status_group.heading_small[0].text
-  second_heading = help_with_status_group.heading_small[1].text
+  first_heading = form_group_3(1).details_content.heading_small[0]
+  second_heading = form_group_3(1).details_content.heading_small[1]
 
-  expect(first_heading).to have_content 'Choose ‘married or living with someone'
-  expect(second_heading).to have_content 'Choose ‘single’'
-  expect(help_with_status_group.ul[0].li.count).to eq 5
-  expect(help_with_status_group.ul[1].li.count).to eq 5
+  expect(first_heading.text).to have_content 'Choose ‘married or living with someone'
+  expect(second_heading.text).to have_content 'Choose ‘single’'
+  expect(form_group_3(1).details_content.ul[0].li.count).to eq 5
+  expect(form_group_3(1).details_content.ul[1].li.count).to eq 5
 end

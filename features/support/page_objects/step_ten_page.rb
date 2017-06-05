335d0fb0d397205bcdf68ce02cfaaec7a19cb6ac
Alexa <alexaballantine@Alexas-MacBook-Pro.local>
2017-06-05 16:52:59 +0100

    RST-329 removed unused selectors

--- features/support/page_objects/step_ten_page.rb
+++ features/support/page_objects/step_ten_page.rb
@@ -1,15 +1,9 @@
 class StepTenPage < BasePage
   section :content, '#content' do
-    element :page_header, 'h1'
     section :text, '.text' do
-      elements :p, 'p'
       elements :li, 'li'
-      elements :hint, '.hint'
     end
     sections :form_group, '.form-group' do
-      element :h3, 'h3'
-      elements :block_label, '.block-label'
-      elements :input, '.block-label input'
       element :income_amount, '#income_amount_amount'
       element :continue_button, '.button'
     end

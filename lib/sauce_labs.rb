42ea0cab3f621a6b7a78aa44830f74dae6250c94
Alexa <alexaballantine@Alexas-MacBook-Pro.local>
2017-06-26 13:56:51 +0100

    RST-364 added sample code from sauce labs site

--- lib/sauce_labs.rb
+++ lib/sauce_labs.rb
@@ -1,2 +1,17 @@
-driver = Selenium::WebDriver.for :remote, :url => sauce_endpoint, :desired_capabilities => caps
-sauce_endpoint = "http://aballantine:c09c4ae6-c68f-419d-b09a-922404c5bb23@ondemand.saucelabs.com:80/wd/hub"
+require "selenium/webdriver"
+
+caps = {
+    :platform => "Windows 7",
+    :browserName => "Chrome",
+    :version => "45"
+}
+
+driver = Selenium::WebDriver.for(:remote,
+                                 :url => "http://aballantine:c09c4ae6-c68f-419d-b09a-922404c5bb23@ondemand.saucelabs.com:80/wd/hub",
+                                 :desired_capabilities => caps)
+
+driver.get('http://saucelabs.com/test/guinea-pig')
+
+puts "title of webpage is: #{driver.title()}"
+
+driver.quit()

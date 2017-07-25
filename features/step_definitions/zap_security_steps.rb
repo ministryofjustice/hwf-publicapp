e7418ffaac6cf72007ad118b96be1027d0d9745f
Alexa <alexa.ballantine@gmail.com>
2017-07-25 13:36:04 +0100

    RST-436 removed expectations to speed up test

--- features/step_definitions/zap_security_steps.rb
+++ features/step_definitions/zap_security_steps.rb
@@ -3,28 +3,44 @@ Given(/^I launch owasp zap for a scan$/) do
 end
 
 When(/^I take the happy path through steps 1 to 20$/) do
-  step 'I visit the page for step one'
-  step 'I enter a valid form number'
-  step 'I select no to have you already paid the fee?'
-  step 'I select single'
-  step 'I select £0 to £2,999'
-  step 'I select no to do you receive any of the following benefits?'
-  step 'I select no to do you have any children'
-  step 'after selecting working tax credit from income list on step eight'
-  step 'I click continue'
-  step 'I select between'
-  step 'I enter a valid total monthly income'
-  step 'I select no to are you paying a fee for a probate case'
-  step 'I select no to do you have a case, claim or notice to pay number'
-  step 'I enter a valid national insurance number'
-  step 'I enter a valid date of birth'
-  step 'I enter my full name'
-  step 'I enter my address with postcode'
-  step 'I click continue'
-  step 'I click submit application and continue'
-  step 'I press the continue button'
-  step 'I click the finish application button'
-  step 'I am taken to the thank you page'
+  step_one_page.load_page
+  group_one(0).form_name_identifier.set('notice to appeal')
+  common_page.continue_button.click
+  group_two(0).fee_paid_false.click
+  common_page.continue_button.click
+  group_three(0).marital_status_married_false.click
+  common_page.continue_button.click
+  group_four(0).savings_and_investment_choice_less.click
+  common_page.continue_button.click
+  group_six(0).benefits_false.click
+  common_page.continue_button.click
+  group_seven(0).dependent_children_false.click
+  common_page.continue_button.click
+  step_eight_page.content.form_group[0].block_label[2].click
+  common_page.continue_button.click
+  step_nine_page.content.form_group[0].income_range_between.click
+  common_page.continue_button.click
+  step_ten_page.content.form_group[0].income_amount.set(1900)
+  common_page.continue_button.click
+  step_eleven_page.probate_case_false.click
+  common_page.continue_button.click
+  step_twelve_page.number_false.click
+  common_page.continue_button.click
+  step_thirteen_page.national_insurance_number.set('JL806367D')
+  common_page.continue_button.click
+  step_fourteen_page.dob_date_of_birth.set(Time.zone.today - 34.years)
+  common_page.continue_button.click
+  step_fifteen_page.first_name.set('Sally')
+  step_fifteen_page.last_name.set('Smith')
+  common_page.continue_button.click
+  step_sixteen_page.address.set('102 Petty France, London')
+  step_sixteen_page.postcode.set('SW1H 9AJ')
+  common_page.continue_button.click
+  common_page.continue_button.click
+  step_eighteen_page.submit_button.click
+  common_page.button.click
+  common_page.button.click
+  expect(current_path).to eq '/'
   sleep 20
 end
 

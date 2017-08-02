d742f7861561680e0eb4120f6fc6a9e57ff925ad
Alexa <alexa.ballantine@gmail.com>
2017-08-02 17:19:49 +0100

    RST-364 automated tests running in browser but need to fix syntax

--- features/support/sauce_labs.rb
+++ features/support/sauce_labs.rb
@@ -1,17 +1,17 @@
-require "selenium/webdriver"
+# require "selenium/webdriver"
 
-caps = {
-    :platform => "Windows 7",
-    :browserName => "Chrome",
-    :version => "45"
-}
+# caps = {
+#     :platform => "Windows 10",
+#     :browser_name => "Chrome",
+#     :version => "59"
+# }
 
-driver = Selenium::WebDriver.for(:remote,
-                                 :url => "http://aballantine:c09c4ae6-c68f-419d-b09a-922404c5bb23@ondemand.saucelabs.com:80/wd/hub",
-                                 :desired_capabilities => caps)
+# driver = Selenium::WebDriver.for(:remote,
+#                                  :url => "http://aballantine:c09c4ae6-c68f-419d-b09a-922404c5bb23@ondemand.saucelabs.com:80/wd/hub",
+#                                  :desired_capabilities => caps)
 
-driver.get('http://localhost:3000')
+# driver.get('http://localhost:3000')
 
-puts "title of webpage is: #{driver.title()}"
+# puts "title of webpage is: #{driver.title()}"
 
-driver.quit()
+# driver.quit()
--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -1,2 +1,2 @@
 require 'webmock/cucumber'
-WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.disable_net_connect!(allow_localhost: true, allow: 'ondemand.saucelabs.com')

589382396c4f82ade0bf6f819fbfce1a1215c37b
Leonidas Apostolidis <leoapost@gmail.com>
2017-11-07 14:51:57 +0000

    remove hardcoded zap conf from tests

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,7 +3,3 @@ Before('@hwf_submit_application') do
   response = { result: true, message: 'HWF-000-000' }
   stub_request(:post, "#{url}/api/submissions").to_return(status: 200, body: response.to_json)
 end
-
-Before('@zap') do
-  IO.popen('/Applications/ZAP\ 2.6.0.app/Contents/Java/zap.sh -daemon')
-end

d011f60e6f37c8d9a76c6dbe19278344d4715294
Alexa <alexa.ballantine@gmail.com>
2017-11-21 12:15:33 +0000

    improved readability of tests for step one

--- features/step_definitions/step_one_steps.rb
+++ features/step_definitions/step_one_steps.rb
@@ -2,21 +2,21 @@ def group_one(index)
   step_one_page.content.new_form_name.form_group[index]
 end
 
-Given(/^I visit the page for step one$/) do
+Given(/^I am on the page for step one$/) do
   step_one_page.load_page
 end
 
-When(/^I enter a valid form number$/) do
+When(/^I submit the form with a valid form number$/) do
   group_one(0).form_name_identifier.set('C100')
   common_page.continue_button.click
 end
 
-When(/^I enter a valid form name$/) do
+When(/^I submit the form with a valid form name$/) do
   group_one(0).form_name_identifier.set('notice to appeal')
   common_page.continue_button.click
 end
 
-When(/^I select I don’t know the form name or number$/) do
+When(/^I submit the form with I don’t know the form name or number checked$/) do
   label = group_one(1).block_label[0]
 
   expect(label.text).to eq 'I don’t know the form name or number'
@@ -25,16 +25,16 @@ When(/^I select I don’t know the form name or number$/) do
   common_page.continue_button.click
 end
 
-When(/^I leave the form name or number blank$/) do
+When(/^I try to submit the form with the form name or number left blank$/) do
   common_page.continue_button.click
 end
 
-Then(/^I should see step one hint$/) do
-  expect(step_one_page.content.hint.text).to have_content 'Enter the court or tribunal form number'
+Then(/^I should see form hint '([^\"]*)'$/) do |hint|
+  expect(step_one_page.content.hint.text).to have_content hint
 end
 
-Then(/^I should see step one label$/) do
-  expect(group_one(0).label.text).to eq 'Form name or number'
+Then(/^I should see form label '([^\"]*)'$/) do |label|
+  expect(group_one(0).label.text).to eq label
 end
 
 Then(/^I see more information about the form name or number$/) do
--- features/step_one.feature
+++ features/step_one.feature
@@ -2,8 +2,8 @@
 
 Feature: Step one page
 
-  Background: Navigating to the page for step one
-    Given I visit the page for step one
+  Background: Page one
+    Given I am on the page for step one
 
     Scenario: Displays step number
       Then I should see step 1 of 20
@@ -12,25 +12,25 @@ Feature: Step one page
       Then I should see 'What court or tribunal fee do you need help with?' header
 
     Scenario: Displays hint
-      Then I should see step one hint
+      Then I should see form hint 'Enter the court or tribunal form number'
 
     Scenario: Displays label
-      Then I should see step one label
+      Then I should see form label 'Form name or number'
 
     Scenario: Entering valid form number
-      When I enter a valid form number
+      When I submit the form with a valid form number
       Then I am taken to step 2 fee
 
     Scenario: Entering a valid form name
-      When I enter a valid form name
+      When I submit the form with a valid form name
       Then I am taken to step 2 fee
 
     Scenario: Leaving form name or number blank
-      When I leave the form name or number blank
+      When I try to submit the form with the form name or number left blank
       Then I should see 'Enter the form name or number' error message
 
     Scenario: Selecting I don’t know the form name or number
-      When I select I don’t know the form name or number
+      When I submit the form with I don’t know the form name or number checked
       Then I am taken to step 2 fee
 
     Scenario: Help with form name or number

0d4fb1dce43eec5a4746f9a566be80fee3e25604
Gary Taylor <gary.taylor@hismessages.com>
2017-12-13 08:18:55 +0000

    Phase 5 of moving calculator code to a gem - moving calculator specific capybara selectors

--- vendor/gems/moj_hwf_calculator/lib/moj_hwf/calculator/test/page_objects.rb
+++ vendor/gems/moj_hwf_calculator/lib/moj_hwf/calculator/test/page_objects.rb
@@ -1,3 +1,4 @@
+Dir.glob(File.absolute_path('../capybara_selectors/**/*.rb', __FILE__)).each { |f| require f }
 Dir.glob(File.absolute_path('../sections/**/*.rb', __FILE__)).each { |f| require f }
 require 'moj_hwf/calculator/test/page_objects/en/calculator/base_page'
 Dir.glob(File.absolute_path('../page_objects/en/**/*.rb', __FILE__)).each { |f| require f }

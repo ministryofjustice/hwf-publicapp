0894caf6839af334f9efdd9283480c0f9c7ca2f5
Petr Zaparka <petr@zaparka.cz>
2019-03-07 10:43:27 +0000

    Renaming the probate fee switch method

--- spec/lib/probate_fees_switch_spec.rb
+++ spec/lib/probate_fees_switch_spec.rb
@@ -1,10 +1,10 @@
 RSpec.describe ProbateFeesSwitch do
   subject { described_class }
 
-  describe '#use_probate_fees_changes?' do
+  describe '#disable_probate_fees?' do
     subject do
       Timecop.freeze(current_time) do
-        described_class.use_probate_fees_changes?
+        described_class.disable_probate_fees?
       end
     end
 
@@ -12,12 +12,7 @@ RSpec.describe ProbateFeesSwitch do
       let(:current_time) { probate_fees_release_date }
 
       it { is_expected.to be true }
-    end
 
-    context 'when called before the set date' do
-      let(:current_time) { probate_fees_release_date - 1.day }
-
-      it { is_expected.to be false }
     end
   end
 end

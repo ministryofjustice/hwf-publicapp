b7fb5b35402a6035711097d564d547badc6dd316
Petr Zaparka <petr@zaparka.cz>
2019-04-09 10:51:28 +0100

    Fixing failing cucumber tests

--- features/support/page_objects/dob_page.rb
+++ features/support/page_objects/dob_page.rb
@@ -4,8 +4,10 @@ class DobPage < BasePage
   section :content, '#content' do
     element :step_info, '.step-info', text: 'Step 14 of 20'
     element :dob_header, 'h1', text: 'What is your date of birth?'
-    element :dob_hint, '.hint', text: 'Use this format D D / M M / Y Y Y Y'
-    element :dob_date_of_birth, '#dob_date_of_birth'
+    element :dob_hint, '.hint', text: 'For example, 01 08 2007'
+    element :dob_day_date_of_birth, '#dob_day_date_of_birth'
+    element :dob_month_date_of_birth, '#dob_month_date_of_birth'
+    element :dob_year_date_of_birth, '#dob_year_date_of_birth'
     element :blank_error_link, 'a', text: 'Enter the date in this format DD/MM/YYYY'
     element :blank_error_message, '.error-message', text: 'Enter the date in this format DD/MM/YYYY'
     element :under_age_error_link, 'a', text: 'You must be over 15 to use this service'
@@ -15,17 +17,27 @@ class DobPage < BasePage
   end
 
   def valid_dob
-    content.dob_date_of_birth.set(Time.zone.today - 34.years)
+    age = Time.zone.today - 34.years
+    content.dob_day_date_of_birth.set(age.day)
+    content.dob_month_date_of_birth.set(age.month)
+    content.dob_year_date_of_birth.set(age.year)
     continue
   end
 
   def under_age_dob
-    content.dob_date_of_birth.set(Time.zone.today - 14.years)
+    age = Time.zone.today - 14.years
+    content.dob_day_date_of_birth.set(age.day)
+    content.dob_month_date_of_birth.set(age.month)
+    content.dob_year_date_of_birth.set(age.year)
     continue
   end
 
   def over_age_dob
-    content.dob_date_of_birth.set(Time.zone.today - 150.years)
+    age = Time.zone.today - 150.years
+    content.dob_day_date_of_birth.set(age.day)
+    content.dob_month_date_of_birth.set(age.month)
+    content.dob_year_date_of_birth.set(age.year)
+
     continue
   end
 end

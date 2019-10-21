9f712bdca7eef662ce814c9742e57d0b6b7ec465
Petr Zaparka <petr@zaparka.cz>
2019-10-21 10:30:36 +0100

    Fixing some tests

--- spec/support/shared_examples/cache_suppress_headers.rb
+++ spec/support/shared_examples/cache_suppress_headers.rb
@@ -4,6 +4,17 @@ RSpec.shared_examples 'cache suppress headers' do
       'Cache-Control' => 'no-cache, no-store, max-age=0, must-revalidate',
       'Pragma' => 'no-cache',
       'Expires' => 'Fri, 01 Jan 1990 00:00:00 GMT'
+      "X-Frame-Options"=>"SAMEORIGIN",
+      "X-XSS-Protection"=>"1; mode=block",
+      "X-Content-Type-Options"=>"nosniff",
+      "X-Download-Options"=>"noopen",
+      "X-Permitted-Cross-Domain-Policies"=>"none",
+      "Referrer-Policy"=>"strict-origin-when-cross-origin",
+      "Content-Type"=>"text/html; charset=utf-8",
+      "Cache-Control"=>"no-cache, no-store",
+      "Pragma"=>"no-cache",
+      "Expires"=>"Fri, 01 Jan 1990 00:00:00 GMT"
+
     )
   end
 end

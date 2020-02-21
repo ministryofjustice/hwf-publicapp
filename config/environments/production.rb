24714afba21c23029de4682b6c15888e1f1e2706
Leonidas Apostolidis <leoapost@gmail.com>
2020-02-21 13:48:10 +0000

    Re-arrange the code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+  config.ssl_options = { hsts: { expires: 1.year, preload: true } }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -88,8 +89,4 @@ Rails.application.configure do
 
   # config.serve_static_files = true
   # config.assets.prefix = '/myassets'
-
-  config.force_ssl = true
-  config.ssl_options = { hsts: { expires: 1.year, preload: true } }
-
 end

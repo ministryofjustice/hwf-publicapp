46b51fa33ab26c6f0a71864e490f2ce239ddb19c
Leonidas Apostolidis <leoapost@gmail.com>
2020-03-11 14:34:34 +0000

    Fix rubocop issues

--- app/mailers/service_now_mailer.rb
+++ app/mailers/service_now_mailer.rb
@@ -3,9 +3,9 @@ class ServiceNowMailer < ActionMailer::Base
 
   def ask_for_help_email(form)
     mail(to: Settings.mail.service_now,
-      reply_to: "#{form.name} <#{form.email}>",
-      subject: "[Ask-for-Help] #{form.name} has requested technical assistance",
-      content_type: "text/plain",
-      body: form.description)
+         reply_to: "#{form.name} <#{form.email}>",
+         subject: "[Ask-for-Help] #{form.name} has requested technical assistance",
+         content_type: "text/plain",
+         body: form.description)
   end
 end

71240f0aa19cb750dee41e51d7b178e006e98a18
Alexa <alexa.ballantine@gmail.com>
2020-03-18 16:30:57 +0000

    rst-2523 cucumber tests for validation for home office number

--- features/support/page_objects/home_office_page.rb
+++ features/support/page_objects/home_office_page.rb
@@ -9,8 +9,10 @@ class HomeOfficePage < BasePage
     element :user_paper_form, 'p', text: "If you don't have either National Insurance number, or a Home Office reference number, you'll need to fill in the paper application form."
     element :paper_form_link, 'a', text: 'paper application form'
     element :help_text, 'li', text: 'Your Home Office reference number can be found on any correspondence you have received from the Home Office'
-    element :error_link, '.error-link', text: 'Enter your Home Office reference number'
-    element :error_message, '.govuk-error-message', text: 'Enter your Home Office reference number'
+    element :error_link, '.error-link', text: 'Enter a valid Home Office reference number'
+    element :error_message, '.govuk-error-message', text: 'Enter a valid Home Office reference number'
+    element :blank_error_link, '.error-link', text: 'Enter your Home Office reference number'
+    element :blank_error_message, '.govuk-error-message', text: 'Enter your Home Office reference number'
   end
 
   def to_home_office_page

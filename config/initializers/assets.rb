424c7d3eab20182008d6708e9e718052b6864a3f
Petr Zaparka <petr@zaparka.cz>
2020-11-18 13:54:05 +0000

    Updating paths to icons and fixing govuk path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,18 +1,25 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '1.1'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('assets', 'fonts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w[*.png *.ico]
-Rails.application.config.assets.precompile += %w[.svg .eot .woff .ttf]
+Rails.application.config.assets.precompile += %w[.svg .eot .woff .ttf .woff2 .woff]
 Rails.application.config.assets.precompile += %w[confirmation-print.css ie8.css ie7.css]
+Rails.application.config.assets.precompile += ['govuk-frontend/govuk/all.css']
+Rails.application.config.assets.precompile += ['govuk-frontend/govuk/all.js']
+Rails.application.config.assets.precompile += ['govuk-frontend/govuk/all-ie8.css']
+Rails.application.config.assets.precompile += ['govuk-frontend/govuk/fonts/*']
+Rails.application.config.assets.precompile += ['images/*']
+
+
+

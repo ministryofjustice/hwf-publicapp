9f7ff6230af358f0b6656dac66e865bcf388e36b
Petr Zaparka <petr@zaparka.cz>
2021-04-19 16:00:50 +0100

    Adding feature tests

--- features/support/page_objects/footer_page.rb
+++ features/support/page_objects/footer_page.rb
@@ -1,5 +1,6 @@
 class FooterPage < BasePage
   section :content, '.govuk-footer.no-print' do
     element :footer_links, '.govuk-footer__inline-list'
+    element :cookie_link, 'a', text: 'Cookies'
   end
 end

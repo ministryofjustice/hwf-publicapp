c5a05a1966554b39b76915f60bcebff31bbce861
Petr Zaparka <petr@zaparka.cz>
2021-04-19 14:56:38 +0100

    cookies banner logic and templates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  protect_from_forgery only: :update
+
   def index; end
 
   def checklist; end
@@ -8,4 +10,37 @@ class HomeController < ApplicationController
   def accessibility_statement; end
 
   def privacy_policy; end
+
+  def cookies
+    @form = Forms::Cookie::SettingForm.new(
+      request: request
+    )
+  end
+
+  def update
+    Forms::Cookie::SettingForm.new(
+      cookie_setting: cookie_setting,
+      response: response
+    ).save
+    flash[:cookie_notification] = cookie_notification
+    redirect_to request.referer
+  end
+
+  private
+
+  def cookie_setting
+    params['cookie_setting'].
+      permit(:cookie_setting).
+      to_h.
+      fetch(:cookie_setting)
+  end
+
+  def cookie_notification
+    if params[:cookie_banner].present?
+      cookie_setting
+    else
+      true
+    end
+  end
+
 end
---
+++ spec/models/forms/cookie/setting_form_spec.rb
@@ -0,0 +1,120 @@
+require 'rails_helper'
+
+RSpec.describe Forms::Cookie::SettingForm do
+  subject { described_class.new(cookie_setting: value, request: request, response: response) }
+
+  let(:value) { 'true' }
+  let(:request) { nil }
+  let(:response) { nil }
+
+  before do
+    Timecop.freeze(Time.local(1990))
+  end
+
+  after do
+    Timecop.return
+  end
+
+  describe '.choices' do
+    it 'returns the relevant choices' do
+      expect(described_class.choices).to eq(%w[true false])
+    end
+  end
+
+  describe '.cookie_setting' do
+    context 'when no value was set and no request cookie present' do
+      let(:value) { nil }
+      let(:request) { nil }
+
+      it 'returns default value "no"' do
+        expect(subject.cookie_setting).to eq(Forms::Cookie::SettingForm::COOKIES_VALUES[:no])
+      end
+    end
+
+    context 'when no value was set and cookie present' do
+      let(:value) { nil }
+      let(:request) { instance_double('ActionDispatch::Request', cookies: { Forms::Cookie::SettingForm::COOKIE_NAME => 'true' }) }
+
+      it 'returns value hold in cookie' do
+        expect(subject.cookie_setting).to eq('true')
+      end
+    end
+
+    it 'returns set value when present' do
+      expect(subject.cookie_setting).to eq(value)
+    end
+  end
+
+  describe '.accepted?' do
+    context 'when cookie_setting is "yes"' do
+      let(:value) { 'true' }
+
+      it 'returns true' do
+        expect(subject).to be_accepted
+      end
+    end
+
+    context 'when cookie_setting is "no"' do
+      let(:value) { 'false' }
+
+      it 'returns false' do
+        expect(subject).not_to be_accepted
+      end
+    end
+  end
+
+  describe '.preference_set?' do
+    context 'when cookie set to NO' do
+      let(:cookie_value_no) { Forms::Cookie::SettingForm::COOKIES_VALUES[:no].to_s }
+      let(:request) { instance_double('ActionDispatch::Request', cookies: { Forms::Cookie::SettingForm::COOKIE_NAME => cookie_value_no }) }
+
+      specify { expect(subject).to be_preference_set }
+    end
+
+    context 'when cookie set to YES' do
+      let(:cookie_value_yes) { Forms::Cookie::SettingForm::COOKIES_VALUES[:yes].to_s }
+      let(:request) { instance_double('ActionDispatch::Request', cookies: { Forms::Cookie::SettingForm::COOKIE_NAME => cookie_value_yes }) }
+
+      specify { expect(subject).to be_preference_set }
+    end
+
+    context 'when cookie has wrong value' do
+      let(:request) { instance_double('ActionDispatch::Request', cookies: { Forms::Cookie::SettingForm::COOKIE_NAME => '' }) }
+
+      specify { expect(subject).not_to be_preference_set }
+    end
+
+    context 'when cookie missing' do
+      let(:request) { instance_double('ActionDispatch::Request', cookies: {}) }
+
+      specify { expect(subject).not_to be_preference_set }
+    end
+  end
+
+  describe '#save' do
+    let(:response) { instance_double('ActionDispatch::Response') }
+
+    before do
+      allow(response).to receive(:delete_cookie).with(Forms::Cookie::SettingForm::COOKIE_NAME, {})
+      allow(response).to receive(:set_cookie)
+    end
+
+    it 'save cookie preference' do
+      expect(subject.save).to be true
+    end
+
+    it 'set value and expire time' do
+      subject.save
+      expect(response).to have_received(:set_cookie).with(
+        Forms::Cookie::SettingForm::COOKIE_NAME,
+        value: value,
+        expires: 1.year.from_now
+      )
+    end
+
+    it 'delete previos settings' do
+      subject.save
+      expect(response).to have_received(:delete_cookie).with(Forms::Cookie::SettingForm::COOKIE_NAME, {})
+    end
+  end
+end
